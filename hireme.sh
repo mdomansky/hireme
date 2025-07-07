8f33ddce3e147f744e98f541a8158774cf2b318d
mdomansky <mx.mixer@gmail.com>
2025-07-07 17:53:59 +0200

    add git pull

--- hireme.sh
+++ hireme.sh
@@ -8,13 +8,14 @@ for coord in "${COMMITS[@]}"; do
   DAY=${coord##*:}
 
   # Найдём первое воскресенье года и прибавим WEEK * 7 + DAY дней
-  START_DATE="2024-07-06" #sunday
+  START_DATE="2025-07-06" #sunday
   COMMIT_DATE=$(date -d "$START_DATE + $((WEEK * 7 + DAY)) days" +%Y-%m-%d)
 
   # Проверяем: если сегодня эта дата — делаем коммиты
   TODAY=$(date +%Y-%m-%d)
 
   if [[ "$COMMIT_DATE" == "$TODAY" ]]; then
+      git pull origin main
     for i in {1..25}; do
       echo "$RANDOM" > random.txt
       git add random.txt
